51c21cdeb0d2268913cc2249bc4a6fb580c25aa5
David Sotomayor <david.elijah.sotomayor@gmail.com>
2020-11-21 13:17:57 -0600

    render todos

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+function renderTodos() {
+    for (var i = 0; i < todos.length; i++) {
+        // create li element
+        var li = document.createElement('li')
+        // set the text content to an li
+        li.innerText = todos[i]
+        // append li to todo-list
+        todoList.appendChild(li);
+        // todoCountSpan should show the total count
+    }
+}
+
+renderTodos()
